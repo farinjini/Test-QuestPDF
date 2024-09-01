0799f02d37209a41d586d1f1f0dc55ae663b173e
Alhassan Abdulkadir <farinjini@gmail.com>
2024-09-01 20:06:24 +0100

    Begin Invoice Document Template

---
+++ src/Test-QuestPDF/Templates/InvoiceDocument.cs
@@ -0,0 +1,47 @@
+﻿using QuestPDF.Infrastructure;
+using Test_QuestPDF.Models;
+
+namespace Test_QuestPDF.Templates;
+
+public class InvoiceDocument : IDocument
+{
+    public InvoiceModel Model { get; }
+
+    public InvoiceDocument(InvoiceModel model)
+    {
+        Model = model;
+    }
+    
+    public DocumentMetadata GetMetadata()
+    {
+        return new DocumentMetadata
+        {
+            Title = "KEDCO Invoice",
+            Author = "Deckstream",
+            Keywords = "Bill, Customer, KEDCO Bill",
+            Producer = "Venn Technology Limited",
+            Creator = "Deckstream",
+            Subject = "Deckstream Bill",
+            CreationDate = DateTimeOffset.Now
+        };
+    }
+    
+    public DocumentSettings GetSettings()
+    {
+        return new DocumentSettings
+        {
+            CompressDocument = true,
+            ContentDirection = ContentDirection.LeftToRight,
+            PdfA = true,
+            ImageCompressionQuality = ImageCompressionQuality.High,
+            ImageRasterDpi = 300
+        };
+    }
+    
+    public void Compose(IDocumentContainer container)
+    {
+        throw new NotImplementedException();
+    }
+    
+    
+}
