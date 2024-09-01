9f5e4077662e0ac7a9111dbac9ee6ea987a47555
Alhassan Abdulkadir <farinjini@gmail.com>
2024-09-01 19:56:14 +0100

    Update Models

--- src/Test-QuestPDF/Models/Address.cs
+++ src/Test-QuestPDF/Models/Address.cs
@@ -2,5 +2,10 @@
 
 public class Address
 {
-    
+    public string CompanyName { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public object Email { get; set; }
+    public string Phone { get; set; }
 }
--- src/Test-QuestPDF/Models/InvoiceModel.cs
+++ src/Test-QuestPDF/Models/InvoiceModel.cs
@@ -2,5 +2,13 @@
 
 public class InvoiceModel
 {
-    
+    public int InvoiceNumber { get; set; }
+    public DateTime IssueDate { get; set; }
+    public DateTime DueDate { get; set; }
+
+    public Address SellerAddress { get; set; }
+    public Address CustomerAddress { get; set; }
+
+    public List<OrderItem> Items { get; set; }
+    public string Comments { get; set; }
 }
