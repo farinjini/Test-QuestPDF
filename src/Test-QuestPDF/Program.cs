1ba676f64108d4b5b7f7253d427830bf2b7f66ae
Alhassan Abdulkadir <farinjini@gmail.com>
2024-09-01 21:00:26 +0100

    WIP

--- src/Test-QuestPDF/Program.cs
+++ src/Test-QuestPDF/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace Test_QuestPDF;
+﻿using QuestPDF;
+using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
+using Test_QuestPDF.Data;
+using Test_QuestPDF.Models;
+using Test_QuestPDF.Templates;
+
+namespace Test_QuestPDF;
 
 class Program
 {
+    public Program()
+    {
+        //QuestPDF.Settings.License = LicenseType.Community;
+    }
+    
     static void Main(string[] args)
     {
+        Settings.License = LicenseType.Community;
+
+        var data = new List<InvoiceModel>();
+        
+        data.Add(InvoiceDocumentDataSource.GetInvoiceDetails());
+        data.Add(InvoiceDocumentDataSource.GetInvoiceDetails());
+        data.Add(InvoiceDocumentDataSource.GetInvoiceDetails());
+        
+        var model = InvoiceDocumentDataSource.GetInvoiceDetails();
+        var document = new InvoiceDocument(data);
+        
+        document.GeneratePdfAndShow();
+        
         Console.WriteLine("Hello, World!");
     }
 }
